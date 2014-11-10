6f47eadf133f90655e4a3f62222fac3ce1b203ce
liaogang <ynwlgh@hotmail.com>
2014-11-10 17:47:52 +0600

    sqrt

--- Sqrt/Sqrt/Sqrt.cpp
+++ Sqrt/Sqrt/Sqrt.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-
+using namespace std;
 
 /**
  Implement int sqrt(int x).
@@ -15,17 +15,31 @@
  Compute and return the square root of x.
  */
 
-
-int sqrt(int x) {
+///@see http://en.wikipedia.org/wiki/Newton%27s_method
+int sqrt(int m)
+{
+    double a = m/2.0;
     
+    double last=0;
+    while (a!=last)
+    {
+        last=a;
+        a=(a+m/a)/2;
+    }
+    
+    return a;
 }
 
 
 int main(int argc, const char * argv[])
 {
-
-    
-    
+    cout<< sqrt(1) <<endl;
+    cout<< sqrt(0) <<endl;
+    cout<< sqrt(4) <<endl;
+    cout<< sqrt(9) <<endl;
+    cout<< sqrt(16) <<endl;
+    cout<< sqrt(2147395599) <<endl;
+    //expected: 46339
     
     return 0;
 }
